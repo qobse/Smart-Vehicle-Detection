d51385bcaf4b82edff2615f34310ff2b13581fa3
Yaqoob Yaghoubi <yaqoob@recogine.com>
2022-07-13 12:14:21 +0800

    Add vehicle counter

--- vehicle_detection/vehicle_detection.py
+++ vehicle_detection/vehicle_detection.py
@@ -9,6 +9,7 @@ from config_helper import ReadConfigFile
 from vehicle_detection.frame_grabber import *
 from vehicle_detection.image_helper import *
 from loguru import logger
+from scipy import spatial
 import numpy as np
 
 import cv2
@@ -96,8 +97,8 @@ class YoloLpd():
             conf = confs[i]
             color = colors[i]
             cv2.putText(img, label + " " + str(format(conf, ".2f")), (x, y - 5),
-                        font, 4, color, 4)
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 5)
+                        font, 2, color, 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
         return img
 
     def crop_img(self, img, box):
@@ -161,3 +162,151 @@ class YoloLpd():
 
             boxes, confs, class_ids = self.nms(boxes, confs, class_ids)
             return boxes, confs, class_ids
+
+
+class VehicleCounter(YoloLpd):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.FRAMES_BEFORE_CURRENT = 10
+        self.LABELS = YoloLpd().get_labels()
+
+    def display_vehicle_count(self, frame, vehicle_count):
+        cv2.putText(
+            frame,  #Image
+            'Detected Vehicles: ' + str(vehicle_count),  #Label
+            (20, 20),  #Position
+            cv2.FONT_HERSHEY_SIMPLEX,  #Font
+            0.8,  #Size
+            (0, 0xFF, 0),  #Color
+            2,  #Thickness
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+        )
+
+    def box_and_line_overlap(self, x_mid_point, y_mid_point, line_coordinates):
+        x1_line, y1_line, x2_line, y2_line = line_coordinates  #Unpacking
+
+        if (x_mid_point >= x1_line and x_mid_point <= x2_line+5) and\
+         (y_mid_point >= y1_line and y_mid_point <= y2_line+5):
+            return True
+        return False
+
+    def display_fps(self, start_time, num_frames):
+        current_time = int(time.time())
+        if (current_time > start_time):
+            os.system('clear')  # Equivalent of CTRL+L on the terminal
+            print("FPS:", num_frames)
+            num_frames = 0
+            start_time = current_time
+        return start_time, num_frames
+
+    def draw_detection_boxes(self, boxes, classIDs, confidences, frame):
+        np.random.seed(42)
+        COLORS = np.random.randint(0,
+                                   255,
+                                   size=(len(self.LABELS), 3),
+                                   dtype="uint8")
+        # ensure at least one detection exists
+        # if len(idxs) > 0:
+        # loop over the indices we are keeping
+        for i, box in enumerate(boxes):
+            # extract the bounding box coordinates
+            (x, y) = (box[0], box[1])
+            (w, h) = (box[2], box[3])
+
+            # draw a bounding box rectangle and label on the frame
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(self.LABELS[classIDs[i]], confidences[i])
+            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        color, 2)
+            #Draw a green dot in the middle of the box
+            cv2.circle(frame, (x + (w // 2), y + (h // 2)),
+                       2, (0, 0xFF, 0),
+                       thickness=2)
+
+    def initialize_video_writer(
+        self,
+        video_width,
+        video_height,
+        fps,
+        save_path,
+    ):
+        # Getting the fps of the source video
+        sourceVideofps = fps
+        # initialize our video writer
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        return cv2.VideoWriter(save_path, fourcc, sourceVideofps,
+                               (video_width, video_height), True)
+
+    def box_in_previous_frames(self, previous_frame_detections, current_box,
+                               current_detections):
+        centerX, centerY, width, height = current_box
+        dist = np.inf  #Initializing the minimum distance
+        # Iterating through all the k-dimensional trees
+        for i in range(self.FRAMES_BEFORE_CURRENT):
+            coordinate_list = list(previous_frame_detections[i].keys())
+            if len(coordinate_list
+                  ) == 0:  # When there are no detections in the previous frame
+                continue
+            # Finding the distance to the closest point and the index
+            temp_dist, index = spatial.KDTree(coordinate_list).query([
+                (centerX, centerY)
+            ])
+            if (temp_dist < dist):
+                dist = temp_dist
+                frame_num = i
+                coord = coordinate_list[index[0]]
+
+        if (dist > (max(width, height) / 2)):
+            return False
+
+        # Keeping the vehicle ID constant
+        current_detections[(
+            centerX, centerY)] = previous_frame_detections[frame_num][coord]
+        return True
+
+    def count_vehicles(self, boxes, classIDs, vehicle_count,
+                       previous_frame_detections, frame):
+        list_of_vehicles = [
+            "bicycle", "car", "motorbike", "bus", "truck", "train"
+        ]
+        current_detections = {}
+        # ensure at least one detection exists
+        # if len(idxs) > 0:
+        # loop over the indices we are keeping
+        for i, box in enumerate(boxes):
+            # extract the bounding box coordinates
+            (x, y) = (box[0], box[1])
+            (w, h) = (box[2], box[3])
+
+            centerX = x + (w // 2)
+            centerY = y + (h // 2)
+
+            # When the detection is in the list of vehicles, AND
+            # it crosses the line AND
+            # the ID of the detection is not present in the vehicles
+            if (self.LABELS[classIDs[i]] in list_of_vehicles):
+                current_detections[(centerX, centerY)] = vehicle_count
+                if (not self.box_in_previous_frames(previous_frame_detections,
+                                            (centerX, centerY, w, h),
+                                            current_detections)):
+                    vehicle_count += 1
+                    # vehicle_crossed_line_flag += True
+                # else: #ID assigning
+                #Add the current detection mid-point of box to the list of detected items
+                # Get the ID corresponding to the current detection
+
+                ID = current_detections.get((centerX, centerY))
+                # If there are two detections having the same ID due to being too close,
+                # then assign a new ID to current detection.
+                if (list(current_detections.values()).count(ID) > 1):
+                    current_detections[(centerX, centerY)] = vehicle_count
+                    vehicle_count += 1
+
+                #Display the ID at the center of the box
+                cv2.putText(frame, str(ID), (centerX, centerY),\
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, [0,0,255], 2)
+
+        return vehicle_count, current_detections
