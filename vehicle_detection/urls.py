0d17ef07c62d581a151ec012c688ef1650bd837f
Yaqoob Yaghoubi <yaqoob@recogine.com>
2022-07-13 12:16:15 +0800

    Add counter end point to the urls

--- vehicle_detection/urls.py
+++ vehicle_detection/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("camera/", views.render_camera_stream, name="camera"),
     path("lpd/", views.render_detection_video, name="lpd"),
+    path("counter/", views.render_vehicle_counter_video, name="counter"),
 ]
