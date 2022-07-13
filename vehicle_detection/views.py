94701ddc6cea0f13c86119dc077d6b32af28e83f
Yaqoob Yaghoubi <yaqoob@recogine.com>
2022-07-13 12:15:26 +0800

    Add counter function to view

--- vehicle_detection/views.py
+++ vehicle_detection/views.py
@@ -7,12 +7,14 @@ from django.http import StreamingHttpResponse
 from config_helper import ReadConfigFile
 from vehicle_detection.frame_grabber import *
 from vehicle_detection.vehicle_detection import *
+# from vehicle_detection.vehicle_counter import *
 
 from loguru import logger
 
 config = ReadConfigFile().read_config("config.json")
 frame_grabber = RTSPframeGrabber(config.camera_url).start()
 yolo = YoloLpd()
+counter = VehicleCounter()
 
 
 def cam(camera):
@@ -43,6 +45,53 @@ def video_feed():
                b'Content-Type: image/jpeg\r\n\r\n' + img_byte + b'\r\n\r\n')
 
 
+def vehicle_counter():
+    # video_width = frame_grabber.frame_width
+    # video_height = frame_grabber.frame_height
+    # fps = frame_grabber.fps
+
+    previous_frame_detections = [{
+        (0, 0): 0
+    } for i in range(counter.FRAMES_BEFORE_CURRENT)]
+
+    vehicle_count = 0
+
+    while True:
+        boxes, confidences, classIDs = [], [], []
+        vehicle_crossed_line_flag = config.counter_bbox
+
+        img = frame_grabber.latest_frame()
+        if not frame_grabber.grabbed:
+            break
+
+        boxes, confidences, classIDs = yolo.predict_lpd(img)
+
+        vehicle_count, current_detections = counter.count_vehicles(
+            boxes, classIDs, vehicle_count, previous_frame_detections, img)
+
+        counter.display_vehicle_count(img, vehicle_count)
+        logger.info(f"[COUNTER] the number of counted vehicles {vehicle_count}")
+
+        # Updating with the current frame detections
+        #Removing the first frame from the list
+        previous_frame_detections.pop(0)
+        # previous_frame_detections.append(spatial.KDTree(current_detections))
+        previous_frame_detections.append(current_detections)
+
+        # Draw detection box
+        # counter.draw_detection_boxes(boxes, classIDs, confidences, img)
+        yolo.draw_boxes(img, boxes, confidences, classIDs, yolo.get_labels())
+
+        flag, img_enc = cv2.imencode(".jpg", img)
+
+        if not flag:
+            continue
+
+        img_byte = img_enc.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + img_byte + b'\r\n\r\n')
+
+
 def render_camera_stream(request):
     return StreamingHttpResponse(
         cam(frame_grabber),
@@ -52,3 +101,9 @@ def render_camera_stream(request):
 def render_detection_video(request):
     return StreamingHttpResponse(
         video_feed(), content_type="multipart/x-mixed-replace; boundary=frame")
+
+
+def render_vehicle_counter_video(request):
+    return StreamingHttpResponse(
+        vehicle_counter(),
+        content_type="multipart/x-mixed-replace; boundary=frame")
